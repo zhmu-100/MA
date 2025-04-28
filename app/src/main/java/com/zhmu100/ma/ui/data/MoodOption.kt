c862b55608fe190009a075c67bc6abe702792044
Kosty Manuhin <kostymanuhin@gmail.com>
2025-04-28 22:57:53 +0300

    comments

--- app/src/main/java/com/zhmu100/ma/ui/data/MoodOption.kt
+++ app/src/main/java/com/zhmu100/ma/ui/data/MoodOption.kt
@@ -2,6 +2,13 @@ package com.zhmu100.ma.ui.data
 
 import com.zhmu100.ma.domain.model.training.ExerciseReaction
 
+/**
+ * Опция настроения для выбора после тренировки
+ *
+ * @property iconId Идентификатор ресурса иконки
+ * @property description Текстовое описание настроения
+ * @property emotion Тип реакции на упражнение
+ */
 data class MoodOption(
     val iconId: Int,
     val description: String,
--- app/src/main/java/com/zhmu100/ma/ui/data/TrainRowData.kt
+++ app/src/main/java/com/zhmu100/ma/ui/data/TrainRowData.kt
@@ -2,7 +2,12 @@ package com.zhmu100.ma.ui.data
 
 import com.zhmu100.ma.domain.model.training.ExerciseName
 
-
+/**
+ * Данные строки тренировки
+ *
+ * @property exercise Название упражнения
+ * @property value Значение (количество повторений/вес/время)
+ */
 data class TrainRowData(
     val exercise: ExerciseName,
     val value: String

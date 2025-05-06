653681a545daf7142d1d3e8c27a90ea41401e27c
Kosty Manuhin <kostymanuhin@gmail.com>
2025-05-06 22:47:01 +0300

    networkModule

--- app/src/main/java/com/zhmu100/ma/di/NetworkModule.kt
+++ app/src/main/java/com/zhmu100/ma/di/NetworkModule.kt
@@ -6,22 +6,27 @@ import com.zhmu100.ma.domain.api.auth.AuthApiImpl
 import com.zhmu100.ma.domain.api.comments.CommentApi
 import com.zhmu100.ma.domain.api.comments.CommentApiImpl
 import com.zhmu100.ma.domain.api.diet.DietApi
+import com.zhmu100.ma.domain.api.diet.DietApiImpl
 import com.zhmu100.ma.domain.api.diet.DietApiMock
 import com.zhmu100.ma.domain.api.feed.PostApi
 import com.zhmu100.ma.domain.api.feed.PostApiImpl
 import com.zhmu100.ma.domain.api.files.FilesApi
+import com.zhmu100.ma.domain.api.files.FilesApiImpl
 import com.zhmu100.ma.domain.api.files.FilesApiMock
 import com.zhmu100.ma.domain.api.notes.NoteApi
 import com.zhmu100.ma.domain.api.notes.NoteApiImpl
 import com.zhmu100.ma.domain.api.notification.NotificationApi
 import com.zhmu100.ma.domain.api.notification.NotificationApiImpl
 import com.zhmu100.ma.domain.api.profile.ProfileApi
+import com.zhmu100.ma.domain.api.profile.ProfileApiImpl
 import com.zhmu100.ma.domain.api.profile.ProfileApiMock
 import com.zhmu100.ma.domain.api.reaction.ReactionApiImpl
 import com.zhmu100.ma.domain.api.reactions.ReactionApi
 import com.zhmu100.ma.domain.api.statistic.StatisticsApi
+import com.zhmu100.ma.domain.api.statistic.StatisticsApiImpl
 import com.zhmu100.ma.domain.api.statistic.StatisticsApiMock
 import com.zhmu100.ma.domain.api.training.TrainingApi
+import com.zhmu100.ma.domain.api.training.TrainingApiImpl
 import com.zhmu100.ma.domain.api.training.TrainingApiMock
 import org.koin.dsl.module
 
@@ -30,22 +35,22 @@ import org.koin.dsl.module
  * Модуль DI для реализаций API
  */
 val networkModule = module {
-    single { Network.httpClient }
-//    single<ProfileApi> { ProfileApiImpl(get(),  "http://localhost:8080/api/profiles") }
-//    single<FilesApi> { FilesApiImpl(get(),  "http://localhost:8080/api/profiles") }
-//    single<TrainingApi> { TrainingApiImpl(get(), "http://localhost:8080/api/training") }
-//    single<DietApi> { DietApiImpl(get(),  "http://localhost:8080/api/diet") }
-//    single<StatisticsApi> { StatisticsApiImpl(get(), "http://localhost:8080/api/statistics") }
 
-    single<ProfileApi> { ProfileApiMock() }
-    single<FilesApi> { FilesApiMock() }
-    single<TrainingApi> { TrainingApiMock() }
-    single<DietApi> { DietApiMock() }
+    single { Network.init(get()) }
     single<AuthApi> { AuthApiImpl(get(), "http://188.225.77.13:8080/api/auth") }
-    single<NotificationApi> { NotificationApiImpl(get(), "http://localhost:8080/api/notebook") }
-    single<NoteApi> { NoteApiImpl(get(), "http://localhost:8080/api/notebook") }
+//    single<ProfileApi> { ProfileApiImpl(get(),  "http://188.225.77.13:8083/profiles") }
+    single<ProfileApi> { ProfileApiMock() }
+//    single<TrainingApi> { TrainingApiImpl(get(), "http://188.225.77.13:8084/training") }
+    single<TrainingApi> { TrainingApiMock() }
+    single<CommentApi> { CommentApiImpl(get(), "http://188.225.77.13:8085/feed") }
+    single<PostApi> { PostApiImpl(get(), get(), "http://188.225.77.13:8085/feed") }
+    single<ReactionApi> { ReactionApiImpl(get(), "http://188.225.77.13:8085/feed") }
+    single<NoteApi> { NoteApiImpl(get(), "http://188.225.77.13:8086/notebook") }
+    single<NotificationApi> { NotificationApiImpl(get(), "http://188.225.77.13:8086/notebook") }
+//    single<DietApi> { DietApiImpl(get(),  "http://188.225.77.13:8087/diet") }
+    single<DietApi> { DietApiMock() }
+//    single<StatisticsApi> { StatisticsApiImpl(get(), "http://188.225.77.13:8088/statistics") }
     single<StatisticsApi> { StatisticsApiMock() }
-    single<PostApi> { PostApiImpl(get(), get(), "http://localhost:8080/api/feed") }
-    single<CommentApi> { CommentApiImpl(get(), "http://localhost:8080/api/feed") }
-    single<ReactionApi> { ReactionApiImpl(get(), "http://localhost:8080/api/feed") }
+//    single<FilesApi> { FilesApiImpl(get(),  "http://188.225.77.13:8089/files") }
+    single<FilesApi> { FilesApiMock() }
 }

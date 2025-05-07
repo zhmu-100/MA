ca41045eab626a1afb26862fdf618490da8be7fb
Айдар <farhutdinov04@gmail.com>
2025-05-07 11:57:21 +0300

    fix2

--- app/src/main/java/com/zhmu100/ma/di/NetworkModule.kt
+++ app/src/main/java/com/zhmu100/ma/di/NetworkModule.kt
@@ -36,8 +36,8 @@ val networkModule = module {
     single<AuthApi> { AuthApiImpl(get(), "http://188.225.77.13:8080/api/auth") }
     single<ProfileApi> { ProfileApiImpl(get(),  "http://188.225.77.13:8083/profiles", get()) }
 //    single<ProfileApi> { ProfileApiMock() }
-//    single<TrainingApi> { TrainingApiImpl(get(), "http://188.225.77.13:8084/training") }
-    single<TrainingApi> { TrainingApiMock() }
+    single<TrainingApi> { TrainingApiImpl(get(), "http://188.225.77.13:8084/training", get()) }
+//    single<TrainingApi> { TrainingApiMock() }
     single<CommentApi> { CommentApiImpl(get(), "http://188.225.77.13:8085") }
     single<PostApi> { PostApiImpl(get(), get(), "http://188.225.77.13:8085") }
     single<ReactionApi> { ReactionApiImpl(get(), "http://188.225.77.13:8085") }
